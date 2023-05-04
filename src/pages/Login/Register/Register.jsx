be8cea5faa96d173fc916a8ccd8a99a9d08c257d
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-04 11:03:31 +0600

    user registration complete

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import login from '../../../assets/login.jpg'
+import { AuthContext } from '../../../providers/AuthProvider';
+import { updateProfile } from 'firebase/auth';
 
 const Register = () => {
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const [accepted, setAccepted] = useState(false);
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        form.reset();
+        setError('');
+        setSuccess('');
+
+
+        if (password.length < 6) {
+            setError('Password must be 6 characters longer')
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const createdUser = result.user;
+                updateUserData(result.user, name, photo);
+                setSuccess('User created successfully')
+                console.log(createdUser);
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+
+    const handleAccepted = event => {
+        setAccepted(event.target.checked);
+    }
+
+    const updateUserData = (user, name, photo) => {
+        updateProfile(user, {
+            displayName: name, photoURL: photo
+        })
+            .then(() => {
+                console.log('user name updated');
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
     return (
         <Container>
             <div className='row'>
@@ -12,7 +64,7 @@ const Register = () => {
                 </div>
                 <div className='mx-auto mb-5 col-md-4 border border-2 p-4 rounded order-1 shadow'>
                     <h2>Please Register</h2>
-                    <Form>
+                    <Form onSubmit={handleRegister}>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" name='name' placeholder="Your Name" required />
@@ -32,16 +84,25 @@ const Register = () => {
                         </Form.Group>
                         <Form.Group className="mb-1" controlId="formBasicCheckbox">
                             <Form.Check
-
+                                onClick={handleAccepted}
                                 type="checkbox"
                                 name="accept"
                                 label={<>Accept <Link to="/terms" style={{ textDecoration: 'none' }}>Terms and Conditions</Link> </>} />
                         </Form.Group>
-
-                        <Button className='fw-semibold col-12 py-2 my-1 border-0' variant="primary" type="submit" style={{ backgroundColor: "#FF5915", }}>
+                        {
+                            error && <Form.Text className="text-danger">
+                                <span>{error}</span> <br />
+                            </Form.Text>
+                        }
+                        <Button disabled={!accepted} className='fw-semibold col-12 py-2 my-1 border-0' variant="primary" type="submit" style={{ backgroundColor: "#FF5915", }}>
                             Register
                         </Button>
                         <br />
+                        {
+                            success && <Form.Text className="text-success">
+                                <span>{success}</span><br />
+                            </Form.Text>
+                        }
                         <Form.Text className="text-secondary">
                             Already have an account? <Link className=' fw-bold' to="/login" style={{ textDecoration: 'none', color: "#FF5915" }}>Login</Link>
                         </Form.Text>
