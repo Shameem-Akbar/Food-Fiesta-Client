3e435957f1296caedc07f58efeaabcbfc90fc15f
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-04 10:45:43 +0600

    register page design done

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -8,22 +8,20 @@ const Login = () => {
     return (
         <Container>
             <div className='row'>
-                <div className='col-md-6'>
+                <div className='col-md-6 order-2 order-lg-1'>
                     <img className='w-100' src={login} alt="" />
                 </div>
-                <div className='mx-auto m-5 col-md-4 border border-2 p-4 rounded'>
-                    <h2>Please Login</h2>
+                <div className='mx-auto mt-1 mb-5 col-md-4 border border-2 p-4 rounded order-1 shadow'>
+                    <h2 className='mt-1'>Please Login</h2>
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" name='email' placeholder="Enter email" required />
+                            <Form.Control type="email" name='email' placeholder="Enter Email" required />
                         </Form.Group>
-
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" name='password' placeholder="Password" required />
                         </Form.Group>
-
                         <Button className='fw-semibold col-12 py-2 border-0' variant="primary" type="submit" style={{ backgroundColor: "#FF5915", }}>
                             Login
                         </Button>
@@ -40,14 +38,14 @@ const Login = () => {
                             <FaGithub className='mb-1 me-1'></FaGithub> Login with Github
                         </Button>
                         <Form.Text className="text-secondary">
-                            Don&apos;t Have an Account? <Link className='text-danger fw-bold' to="/register" style={{ textDecoration: 'none' }}>Register</Link>
+                            Don&apos;t have an account? <Link className='fw-bold' to="/register" style={{ textDecoration: 'none', color: "#FF5915" }}>Register</Link>
                         </Form.Text>
                     </Form>
                 </div>
 
 
             </div>
-        </Container>
+        </Container >
     );
 };
 
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import login from '../../../assets/login.jpg'
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <Container>
+            <div className='row'>
+                <div className='col-md-6 order-2 order-lg-1'>
+                    <img className='w-100' src={login} alt="" />
+                </div>
+                <div className='mx-auto mb-5 col-md-4 border border-2 p-4 rounded order-1 shadow'>
+                    <h2>Please Register</h2>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" name='name' placeholder="Your Name" required />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPhotoURL">
+                            <Form.Label>Photo URL</Form.Label>
+                            <Form.Control type="text" name='photo' placeholder="Photo URL" required />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name='email' placeholder="Enter Email" required />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name='password' placeholder="Password" required />
+                        </Form.Group>
+                        <Form.Group className="mb-1" controlId="formBasicCheckbox">
+                            <Form.Check
+
+                                type="checkbox"
+                                name="accept"
+                                label={<>Accept <Link to="/terms" style={{ textDecoration: 'none' }}>Terms and Conditions</Link> </>} />
+                        </Form.Group>
+
+                        <Button className='fw-semibold col-12 py-2 my-1 border-0' variant="primary" type="submit" style={{ backgroundColor: "#FF5915", }}>
+                            Register
+                        </Button>
+                        <br />
+                        <Form.Text className="text-secondary">
+                            Already have an account? <Link className=' fw-bold' to="/login" style={{ textDecoration: 'none', color: "#FF5915" }}>Login</Link>
+                        </Form.Text>
+                    </Form>
+                </div>
+            </div>
+        </Container>
     );
 };
 
